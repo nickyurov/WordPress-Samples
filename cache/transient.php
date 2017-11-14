887289c6b9b096404868e0fcf788af0ce01006ac
Nick Yurov <nick.yurov.art@gmail.com>
2017-11-14 17:36:52 +0200

    reformat code

--- cache/transient.php
+++ cache/transient.php
@@ -5,11 +5,11 @@
 
 $some_data = get_transient( 'some_transient_key' );
 
-if( empty( $some_data ) ){
+if ( empty( $some_data ) ) {
 
 	$some_data = get_posts( '...' );
 
-	if( $some_data ) {
+	if ( $some_data ) {
 		set_transient( 'some_transient_key', $some_data, DAY_IN_SECONDS );
 	}
 
